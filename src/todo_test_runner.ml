a28daf1006360bced977dcfa2297e3cfa6932da9
rexim <reximkut@gmail.com>
2017-07-05 12:04:09 +0700

    Add UTs for TodoStream.flatten (#5)

---
+++ src/todo_test_runner.ml
@@ -0,0 +1,23 @@
+open OUnit2
+open TodoStream
+
+let test_todo_stream_flatten test_ctxt =
+  assert_equal (let accum = ref [] in
+                begin
+                  Stream.of_list [Stream.of_list [1; 2; 3];
+                                  Stream.of_list [1; 2; 3];
+                                  Stream.of_list [1; 2; 3]]
+                  |> TodoStream.flatten
+                  |> Stream.iter (fun x -> accum := x :: !accum);
+                  List.rev !accum
+                end)
+               [1; 2; 3;
+                1; 2; 3;
+                1; 2; 3]
+
+let suite =
+  "suite">:::
+    ["test_todo_stream_flatten" >:: test_todo_stream_flatten]
+
+
+let () = run_test_tt_main suite
