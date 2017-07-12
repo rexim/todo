909e4ea33a10080dae02f4a412efc5d10dedbf52
rexim <reximkut@gmail.com>
2017-07-12 15:51:22 +0700

    Implement TodoFile.root_of_git_repo and adjust its usage (#11)

--- src/todoFile.ml
+++ src/todoFile.ml
@@ -10,8 +10,7 @@ let rec files_of_dir_tree path =
   then Sys.readdir path
        |> Array.to_list
        |> List.map (fun file ->
-              [path; file]
-              |> String.concat "/"
+              Filename.concat path file
               |> files_of_dir_tree)
        |> List.flatten
   else [ path ]
@@ -27,9 +26,23 @@ let rec file_stream_of_dir_tree path : string Stream.t =
        |> Sys.readdir
        |> Array.to_list
        |> Stream.of_list
-       |> TodoStream.map file_stream_of_dir_tree
+       |> TodoStream.map (fun name ->
+              name
+              |> Filename.concat path
+              |> file_stream_of_dir_tree)
        |> TodoStream.flatten
   else Stream.of_list [path]
 
-let root_of_git_repo path : string option =
-  failwith "Not implemented yet"
+let rec root_of_git_repo path : string option =
+  let parent_path = Filename.dirname path
+  in if Sys.is_directory path
+     then TodoOption.first_some
+            (path
+             |> Sys.readdir
+             |> Array.to_list
+             |> List.exists (String.equal ".git")
+             |> TodoOption.of_bool path)
+            ((not (String.equal parent_path path))
+             |> TodoOption.of_bool parent_path
+             |> TodoOption.flat_map root_of_git_repo)
+     else root_of_git_repo parent_path
