72871afd3a761a9654793dcd5a944b3763a2905f
rexim <reximkut@gmail.com>
2018-02-01 17:44:08 +0700

    Replace line in a file using temporary intermediate file (#45)

--- src/todoTest.ml
+++ src/todoTest.ml
@@ -1,7 +1,22 @@
+open Batteries
 open OUnit2
+open TodoFile
 
 let suite =
   "suite" >:::
-    [ (* "label" >:: test_function *) ]
+    [ (* "label" >:: test_function *)
+      "replace_line_at_location" >::
+        (fun test_ctxt ->
+          let file = Filename.temp_file "todo" "txt" in
+          [ "foo"; "bar"; "baz" ]
+          |> List.enum
+          |> TodoFile.stream_to_file file;
+          TodoFile.replace_line_at_location { file_path = file
+                                            ; line_number = 1 }
+                                            "hello";
+          (* TODO: assert_equal does not print the values it compares *)
+          assert_equal [ "foo"; "hello"; "baz" ]
+                       (TodoFile.stream_of_lines file
+                        |> List.of_enum)) ]
 
 let () = run_test_tt_main suite
