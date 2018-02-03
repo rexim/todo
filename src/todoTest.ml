c391ef01d88d9da07e526155dc6378d5478de138
rexim <reximkut@gmail.com>
2018-02-03 21:50:52 +0700

    Fix #48

--- src/todoTest.ml
+++ src/todoTest.ml
@@ -1,11 +1,12 @@
 open Batteries
 open OUnit2
 open TodoFile
+open Todo
 
 let suite =
   "suite" >:::
     [ (* "label" >:: test_function *)
-      "replace_line_at_location" >::
+      "TodoFile.replace_line_at_location" >::
         (fun test_ctxt ->
           let file = Filename.temp_file "todo" "txt" in
           [ "foo"; "bar"; "baz" ]
@@ -17,6 +18,22 @@ let suite =
           (* TODO(#47): assert_equal does not print the values it compares *)
           assert_equal [ "foo"; "hello"; "baz" ]
                        (TodoFile.stream_of_lines file
-                        |> List.of_enum)) ]
+                        |> List.of_enum))
+    ; "Todo.todos_of_file" >::
+        (fun test_ctxt ->
+          let file = Filename.temp_file "todo" "txt" in
+          [ "hello"
+          ; "TODO: world"
+          ; "TODO: foo" ]
+          |> List.enum
+          |> TodoFile.stream_to_file file;
+          assert_equal [ Some 1; Some 2 ]
+                       (Todo.todos_of_file file
+                        |> Enum.map (fun todo ->
+                               todo.location
+                               |> BatOption.map (fun l -> l.line_number))
+                        |> List.of_enum
+                        |> List.sort compare))
+    ]
 
 let () = run_test_tt_main suite
